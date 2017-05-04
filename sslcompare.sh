9fe96a41fd839b3edd88edb77c88d1d849ba1c41
Angel Nieves <13354095+Sartron@users.noreply.github.com>
2017-05-04 13:04:33 -0400

    Commit 0.3

--- sslcompare.sh
+++ sslcompare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Active SSL Compare 0.2
+# Active SSL Compare 0.3
 # Written by Angel Nieves - April 30 2017
 # Compares resulting SSL certificate between curl and openssl
 
@@ -9,12 +9,16 @@
 # 1 = Certificates don't match
 # 2 = Invalid arguments supplied
 # 3 = Domain does not resolve
+# 4 = Connection timed out
+# 5 = Connection refused
 
 DOMAIN='';
 PORT='';
 RESULTONLY='';
 
 RESULT='';
+
+TIMEOUT='10';
 
 function ParseDate()
 {
@@ -29,11 +33,19 @@ function ParseDate()
 function Main()
 {
 	# Curl
-	curlErr=$(curl -svI "https://$DOMAIN:$PORT" 2>&1 >/dev/null);
+	curlErr=$(curl -svI "https://$DOMAIN:$PORT" --connect-timeout "$TIMEOUT" 2>&1 >/dev/null);
+	
+	# Abort if connection is made to a URL that won't load
+	if [[ -n $(echo $curlErr | grep -E 'Connection timed out after .+ milliseconds') ]]; then
+		echo "Connection to https://$DOMAIN:$PORT timed out!";
+		return 4;
+	elif [[ -n $(echo $curlErr | grep 'Connection refused') ]]; then
+		echo "Connection to https://$DOMAIN:$PORT was refused!";
+		return 5;
+	fi
+	
 	startDate=$(ParseDate "$(echo "$curlErr" | awk -F': ' '/start date/ {print $2}')");
-	#startDate=$(echo "$curlErr" | awk -F': ' '/start date/ {print $2}');
 	expDate=$(ParseDate "$(echo "$curlErr" | awk -F': ' '/expire date/ {print $2}')");
-	#expDate=$(echo "$curlErr" | awk -F': ' '/expire date/ {print $2}');
 	commonN=$(echo "$curlErr" | awk -F': ' '/common name/ {print $2}');
 	rawgroup=$(echo "$curlErr" | awk -F',O=' '/issuer:/ {print $2}' | awk -F',.=' '{print $1}');
 	parsegroup=$(echo "$rawgroup" | sed 's/"//g'); # Used to remove " characters from cPanel, Inc.
@@ -104,6 +116,9 @@ do
 				DOMAIN=$2;
 			else
 				# Program failed
+				if [[ $RESULTONLY == '1' ]]; then
+					echo 2;
+				fi
 				exit 2;
 			fi
                         shift
@@ -114,6 +129,9 @@ do
                                 PORT=$2;
                         else
 				# Program failed
+                                if [[ $RESULTONLY == '1' ]]; then
+                                        echo 2;
+                                fi
                                 exit 2;
                         fi
 			shift
@@ -132,6 +150,9 @@ done
 if [[ -n $DOMAIN || -n $PORT ]]; then
 	# Make sure domain resolves.
 	if [[ -z $(dig "$DOMAIN" +short) ]]; then
+		if [[ $RESULTONLY == '1' ]]; then
+			echo 3;
+		fi
 		exit 3;
 	fi
 	
