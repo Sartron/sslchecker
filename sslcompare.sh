3bd8b435c5691cdd83eb242f754cf1e6aa89bf0e
Angel Nieves <13354095+Sartron@users.noreply.github.com>
2017-05-18 08:11:41 -0400

    Commit 0.4

--- sslcompare.sh
+++ sslcompare.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-# Active SSL Compare 0.3
-# Written by Angel Nieves - April 30 2017
-# Compares resulting SSL certificate between curl and openssl
+# SSL Checker 0.4
+# Written by Angel Nieves
+# Compares resulting SSL certificate between cURL and OpenSSL
 
 # Exit Codes
 # 0 = Certificates match
 # 1 = Certificates don't match
-# 2 = Invalid arguments supplied
-# 3 = Domain does not resolve
-# 4 = Connection timed out
-# 5 = Connection refused
+# 2 = Certificate(s) expired
+# 3 = Invalid arguments supplied
+# 4 = Domain does not resolve
+# 5 = Connection timed out
+# 6 = Connection refused
 
 DOMAIN='';
 PORT='';
-RESULTONLY='';
+EXPIRED='0';
+RESULTONLY='0';
 
 RESULT='';
 
 TIMEOUT='10';
 
+# Converts exit code into boolean
+# $1 = Exit Code
+# $2 = Reverse Color (boolean of 1 or 0)
+function CodeToBool()
+{
+	if [ $1 == '0' -a $2 == '0' ]; then
+		echo -e "\e[32mTrue\e[0m";
+	elif [ $1 == '0' -a $2 == '1' ]; then
+		echo -e "\e[31mTrue\e[0m";
+	elif [ $1 == '1' -a $2 == '0' ]; then
+		echo -e '\e[31mFalse\e[0m';
+	elif [ $1 == '1' -a $2 == '1' ]; then
+		echo -e '\e[32mFalse\e[0m';
+	fi
+}
+
+# Add 0 to date to ensure it may be compared
+# $1 = Date input
 function ParseDate()
 {
 	if [[ -n $(echo "$1" | grep -E '^.{3}\s{2}[0-9]\s') ]]; then # Mon  0 
@@ -29,134 +49,143 @@ function ParseDate()
 	fi
 }
 
+# Return if date has been passed
+# $1 = Expiration date in 'date' format
+function CheckExpired()
+{
+	curdateNix=$(date +%s);
+	expdateNix=$(date -d "$1" +%s);
+	
+	test $curdateNix -ge $expdateNix;
+	return $?;
+}
+
 # Return whether or not the certificates match
 function Main()
 {
-	# Curl
-	curlErr=$(curl -svI "https://$DOMAIN:$PORT" --connect-timeout "$TIMEOUT" 2>&1 >/dev/null);
+	# Establish cURL and OpenSSL connections
+	curlErr=$(curl -svI "https://$DOMAIN:$PORT" --connect-timeout "$TIMEOUT" 2>&1 > /dev/null); curlCode=$?;
+	opensslOut=$(echo | timeout "$TIMEOUT" openssl s_client -connect "$DOMAIN:$PORT" 2> /dev/null); opensslCode=$?;
 	
-	# Abort if connection is made to a URL that won't load
-	if [[ -n $(echo $curlErr | grep -E 'Connection timed out after .+ milliseconds') ]]; then
+	## Abort if connection was made to a URL that didn't load
+	if [ $curlCode == '28' -o $opensslCode == '124' ]; then
 		echo "Connection to https://$DOMAIN:$PORT timed out!";
 		return 4;
-	elif [[ -n $(echo $curlErr | grep 'Connection refused') ]]; then
+	elif [ $curlCode == '7' -o $opensslCode == '1' ]; then
 		echo "Connection to https://$DOMAIN:$PORT was refused!";
 		return 5;
 	fi
+	# End cURL/OpenSSL connections
 	
+	# cURL and OpenSSL variable dump
 	startDate=$(ParseDate "$(echo "$curlErr" | awk -F': ' '/start date/ {print $2}')");
 	expDate=$(ParseDate "$(echo "$curlErr" | awk -F': ' '/expire date/ {print $2}')");
 	commonN=$(echo "$curlErr" | awk -F': ' '/common name/ {print $2}');
 	rawgroup=$(echo "$curlErr" | awk -F',O=' '/issuer:/ {print $2}' | awk -F',.=' '{print $1}');
-	parsegroup=$(echo "$rawgroup" | sed 's/"//g'); # Used to remove " characters from cPanel, Inc.
-	
-	echo -e "curl Results";
-	echo -e " Common Name: $commonN";
-	echo -e " Organization: $parsegroup";
-	if [[ $rawgroup != $parsegroup ]]; then
-		echo -e "  Raw: $rawgroup";
-	fi
-	echo -e " Start: $startDate\n End: $expDate";
-
-	
-	# OpenSSL
-	opensslOut=$(echo | openssl s_client -connect "$DOMAIN:$PORT" 2> /dev/null);
-	_startDate=$(ParseDate "$(echo | openssl s_client -connect "$DOMAIN:$PORT" 2> /dev/null | openssl x509 -noout -dates | awk -F'=' '/notBefore/ {print $2}')");
-	_expDate=$(ParseDate "$(echo | openssl s_client -connect "$DOMAIN:$PORT" 2> /dev/null | openssl x509 -noout -dates | awk -F'=' '/notAfter/ {print $2}')");
+	parsegroup=$(echo "$rawgroup" | sed 's/"//g'); # Used to remove " characters from "cPanel, Inc."
+	expired=$(CheckExpired "$expDate"; echo $?);
+	_startDate=$(ParseDate "$(echo "$opensslOut" | openssl x509 -noout -dates | awk -F'=' '/notBefore/ {print $2}')");
+	_expDate=$(ParseDate "$(echo "$opensslOut" | openssl x509 -noout -dates | awk -F'=' '/notAfter/ {print $2}')");
 	_commonN=$(echo "$opensslOut" | awk -F'CN=' '/subject/ {print $2}');
 	_group=$(echo "$opensslOut" | awk -F'O=' '/issuer/ {print $2}' | awk -F'/' '{print $1}');
+	_expired=$(CheckExpired "$_expDate"; echo $?);
+	# End variable dump
 	
-	echo -e "\nopenssl Results\n Common Name: $_commonN\n Organization: $_group\n Start: $_startDate\n End: $_expDate";
+	#Output cURL and OpenSSL results
+	echo -e "\e[2mcURL Results\e[0m";
+	echo "Common Name: $commonN";
+	echo "Organization: $parsegroup";
+	echo -e "Expired: $(CodeToBool $expired 1)\n Start: $startDate\n End: $expDate";
+	echo -e "\n\e[2mOpenSSL Results\e[0m";
+	echo "Common Name: $_commonN";
+	echo "Organization: $_group";
+	echo -e "Expired: $(CodeToBool $_expired 1)\n Start: $_startDate\n End: $_expDate";
+	#End cURL and OpenSSL results
 	
-	# Return comparison
-	echo -e "\nResults";
+	# Compare between cURL and OpenSSL output
+	mismatch=$(test $commonN != $_commonN -o "$parsegroup" != "$_group" -o "$startDate" != "$_startDate" -o "$expDate" != "$_expDate"; echo $?);
+	__expired=$(test $EXPIRED == '1' -a $expired == '0' -o $EXPIRED == '1' -a $_expired == '0'; echo $?);
 	
-        if [[ $commonN == $_commonN ]]; then
-                echo " Common Name: True/Good";
-        else
-                echo " Common Name: False/Bad";
-        fi
-	if [[ $parsegroup == $_group ]]; then
-                echo " Organization: True/Good";
-        else
-                echo " Organization: False/Bad";
-	fi
-	if [[ $(echo $startDate | sed 's/ //g') == $(echo $_startDate | sed 's/ //g') ]]; then
-		echo " Start Date: True/Good";
-	else
-		echo " Start Date: False/Bad";
-	fi
-	if [[ $(echo $expDate | sed 's/ //g') == $(echo $_expDate | sed 's/ //g') ]]; then
-                echo " Expiry Date: True/Good";
-        else
-                echo " Expiry Date: False/Bad";
-	fi
-
-
-	if [[ $commonN != $_commonN || $parsegroup != $_group || $(echo $startDate | sed 's/ //g') != $(echo $_startDate | sed 's/ //g') || $(echo $expDate | sed 's/ //g') != $(echo $_expDate | sed 's/ //g') ]]; then
+	printf "\n\e[2mComparison Results\e[0m: ";
+	if [ $mismatch == '0' ]; then
 		# Failure
+		echo 'Different';
 		RESULT='1';
 	else
 		# Success
+		echo -e '\e[32mSame\e[0m';
 		RESULT='0';
 	fi
-	echo "Final Result: $RESULT";
 	
+	echo "Common Name: $(CodeToBool $(test $commonN == $_commonN; echo $?) 0)";
+	echo "Organization: $(CodeToBool $(test "$parsegroup" == "$_group"; echo $?) 0)";
+	echo "Expired: $(CodeToBool $(test $expired == $_expired; echo $?) 0)";
+	echo " Start: $(CodeToBool $(test "$startDate" == "$_startDate"; echo $?) 0)";
+	echo " End: $(CodeToBool $(test "$expDate" == "$_expDate"; echo $?) 0)";
+	# End comparison
+	
+	if [ $__expired == '0' ]; then
+		RESULT='2';
+	fi
 	return $RESULT;
 }
 
 # Main code execution
 while [[ $# -gt 0 ]]
 do
-        arg=$1;
-        case $arg in
-                -d|--domain)
+	arg=$1;
+	case $arg in
+		-d|--domain)
 			_arg=$(echo "$2" | cut -c'1-2');
 			if [[ $_arg != '--' && $_arg != '-' && -n $_arg ]]; then
 				DOMAIN=$2;
 			else
 				# Program failed
-				if [[ $RESULTONLY == '1' ]]; then
+				if [ $RESULTONLY == '1' ]; then
 					echo 2;
 				fi
 				exit 2;
 			fi
-                        shift
-                        ;;
+			shift
+			;;
 		-p|--port)
-                        _arg=$(echo "$2" | cut -c'1-2');
-                        if [[ $_arg != '--' && $_arg != '-' && -n $_arg ]]; then
-                                PORT=$2;
-                        else
+			_arg=$(echo "$2" | cut -c'1-2');
+			if [[ $_arg != '--' && $_arg != '-' && -n $_arg ]]; then
+				PORT=$2;
+			else
 				# Program failed
-                                if [[ $RESULTONLY == '1' ]]; then
-                                        echo 2;
-                                fi
-                                exit 2;
-                        fi
+				if [ $RESULTONLY == '1' ]; then
+					echo 2;
+				fi
+				exit 2;
+			fi
+			shift
+			;;
+		--expired)
+			EXPIRED='1';
 			shift
 			;;
 		--resultonly)
 			RESULTONLY='1';
 			shift
 			;;
-                *)
+		*)
 			# Unknown argument
-                        shift
-                        ;;
-        esac
+			shift
+			;;
+	esac
 done
 
-if [[ -n $DOMAIN || -n $PORT ]]; then
+if [[ -n $DOMAIN && -n $PORT ]]; then
 	# Make sure domain resolves.
-	if [[ -z $(dig "$DOMAIN" +short) ]]; then
-		if [[ $RESULTONLY == '1' ]]; then
+	if [ -z $(dig "$DOMAIN" +short) ]; then
+		if [ $RESULTONLY == '1' ]; then
 			echo 3;
 		fi
 		exit 3;
 	fi
 	
-	if [[ $RESULTONLY == '1' ]]; then
+	if [ $RESULTONLY == '1' ]; then
 		Main > /dev/null;
 		exitcode=$?;
 		echo $exitcode;
